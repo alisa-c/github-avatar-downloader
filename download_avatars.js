499cad444bffc60b1111d67c2838afd421ee4cd4
alisa-c <alisachuvilskaya@gmail.com>
2018-10-29 23:12:46 +0000

    added function content in getRepoContributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,10 @@
 var request = require('request');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+  cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
