0b181a7fb0c2e495b858ed875f5621e121d1877d
alisa-c <alisachuvilskaya@gmail.com>
2018-10-30 00:14:08 +0000

    parsed file and retrieved avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,27 @@
 var request = require('request');
 
+var token = require('./secrets.js')
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
-  cb(err, body);
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${token.GITHUB_TOKEN}`
+    }
+  };
+
+  request(options, function(err, res, body) {
+    var parsedBody = JSON.parse(body);
+    cb(err, parsedBody);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  result.forEach(function(user) {
+    console.log(user.avatar_url);
+  });
 });
 
 
